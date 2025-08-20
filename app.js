7d7dab5229216e2326600e9946af9fce23294f3c
maruf-hussain <engmarufhussain@gmail.com>
2025-08-21 01:13:27 +0600

    load post api

---
+++ app.js
@@ -0,0 +1,62 @@
+function loadUsrers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data))
+};
+
+function displayUsers(data){
+    let users = document.getElementById('users');
+    users.innerHTML = '';
+    data.forEach(user => {
+        let li = document.createElement('li');
+        li.innerText = `${user.name} - ${user.email}`;
+        users.appendChild(li);
+ });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function loadPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(post => displayPost(post))
+}
+
+ 
+function displayPost(post){
+    const p = document.getElementById('post');
+        p.innerHTML= '';
+        post.forEach(post => {
+        let h3 = document.createElement('h3');
+        h3.innerText = `${post.title} - ${post.body}`;
+        p.appendChild(h3);
+
+    })
+    
+}
